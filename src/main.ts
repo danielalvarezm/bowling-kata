0eab3a5cd1689eec57c19bd97eea8798f04ec250
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-05-13 10:38:45 +0100

    Test success: Add all simple points

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-function sayHelloWorld() {
-  return 'Hello World';
+function calculateBowlingScore(frames: string): number {
+  return frames.split(' ')
+    .flatMap((frame) => frame.split(''))
+    .map(Number)
+    .reduce((a, b) => a + b);
 }
 
-export {sayHelloWorld};
+export {calculateBowlingScore};
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,7 +1,18 @@
-import {sayHelloWorld} from '../src/main';
+import {calculateBowlingScore} from '../src/main';
 
-describe('Basic test should', () => {
-  it('say "Hello World"', () => {
-    expect(sayHelloWorld() ).toBe('Hello World');
+/**
+ * TODO: Bowling kata
+ * ! Do not check for valid rolls.
+ * ! Do not check for correct number of rolls and frames.
+ * ! Do not provide scores for intermediate frames.
+ * * Add all simple points.
+ * * Correctly interpret misses.
+ * * Correctly interpret spares.
+ * * Correctly interpret strikes.
+ */
+describe('Bowling kata should', () => {
+  it('Add all simple points', () => {
+    const game = '52 52 52 52 52 52 52 52 52 52';
+    expect(calculateBowlingScore(game) ).toBe(70);
   });
 });
